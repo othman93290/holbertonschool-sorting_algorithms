8845836aef557a9ca9a2d82c1fd5ad4eb172de1d
MrSamy91 <samy.ajouid@gmail.com>
2024-01-16 16:48:53 +0100

    fixing minor betty error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,8 +2,8 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order using Bubble Sort
- *
+ * bubble_sort - Sorts an array of integers
+ * in ascending order using Bubble Sort
  * @array: The array to be sorted
  * @size: Number of elements in the array
  */
--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,9 +1,9 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
- *                       using the Insertion sort algorithm.
- *
+ * insertion_sort_list - Sorts a doubly linked list
+ * of integers in ascending order
+ * using the Insertion sort algorithm.
  * @list: Double pointer to the head of the doubly linked list
  */
 void insertion_sort_list(listint_t **list)
